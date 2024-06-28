d2d0f7a232f0e15f885e421d0e8da6e3fc0d4db9
Adam Sawyer <148652148+AboveCosmic391@users.noreply.github.com>
2024-06-28 09:30:26 -0400

    Add files via upload

--- scripts/testList.js
+++ scripts/testList.js
@@ -1,4 +1,20 @@
-function readBenefits(){
-    console.log(benefitsOfCloudComputing);
+function compileList(){
+    console.log("Benefits of cloud computing length: " + benefitsOfCloudComputing.length);
+    console.log("Design Principles of the cloud length: " + designPrinciplesOfCloud.length);
+
+    // domain 1
+    var benefits = Math.floor(Math.random() * benefitsOfCloudComputing.length); // single benefit item
+
+    var design = Math.floor(Math.random() * designPrinciplesOfCloud.length); // single design question 
+
+
+    var list = [];
+    list.push(benefitsOfCloudComputing[benefits]);
+    list.push(designPrinciplesOfCloud[design]);
+    console.log("List items: " + list);
+
+
+    // var list = benefitsOfCloudComputing;
+    return list;
 }
 //console.log(benefitsOfCloudComputing);
